842785b2f978b2a92bd8870332fc2644f930d43a
ZhouYu <econyz1216@gmail.com>
2017-10-09 01:05:31 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+let data;
+let plotX1, plotX2, plotY1, plotY2;
+
+function preload(){
+  data = loadTable("data/AM_ENPs.csv", "csv", "header");
+}
+
+function setup() {
+  createCanvas(770,400);
+  plotX1 = 120;
+  plotY1 = 50;
+  plotX2 = width - 50;
+  plotY2 = height - 50;
+}
+
+function draw() {
+
+  // background
+  background(224);
+
+  // main graph
+  fill(255);
+  rectMode(CORNERS);
+  noStroke();
+  rect(plotX1, plotY1, plotX2, plotY2);
+
+  // histogram
+  for (let i = 0; i < data.getRowCount(); i++) {
+    fill(255);
+    let y1 = data.getNum(i,1);
+    let x = data.getNum(i,0);
+    rectMode(RADIUS);
+    rect(plotX1+30*x, height/2-150*y1/2, 12, 150*y1/2);
+    fill(#5679C1);
+    let y2 = data.getNum(i,2);
+    rect(plotX1+30*x, height/2+150*y2/2, 12, 150*y2/2);
+  }
+}
